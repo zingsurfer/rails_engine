78098422351934eff2c63d2675a5ba9fe7abc1a8
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-01 22:37:17 -0600

    Fix typo

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 describe 'Merchants API' do
   it 'sends a list of merchants' do
-    create_merchant(:merchant, 4)
+    create_list(:merchant, 4)
 
     get '/api/v1/merchants'
 
     expect(response).to be_successful
-  end 
+  end
 end
