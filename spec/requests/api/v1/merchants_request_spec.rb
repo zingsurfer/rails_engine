ad3507c724b0cc48625efd0616155504797f177b
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-01 23:23:54 -0600

    Add parse method & additional expectation to test for if it sends list of merchants

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -7,5 +7,9 @@ describe 'Merchants API' do
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body)
+
+    expect(merchants.count).to eq(4)
   end
 end
