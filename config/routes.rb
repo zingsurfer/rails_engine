211c19099bcc6ca69249a827033f00348715d9a2
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-02 22:00:17 -0600

    Add route for searching for a single merchant by attributes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        get '/find', to: 'search#show'
+      end
+
       resources :merchants, only: [:index, :show]
     end
   end
