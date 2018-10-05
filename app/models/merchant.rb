135b02180bf09b69318e8c10b9c461f23e326bba
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-10-05 11:20:12 -0600

    adds daniel comments

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,17 +5,17 @@ class Merchant < ApplicationRecord
 
   validates_presence_of :name
 
-  def self.search(params)
-    if params[:name]
-      find_by(name: params[:name])
-    elsif params[:id]
-      find_by(id: params[:id])
-    elsif params[:created_at]
-      find_by(created_at: params[:created_at])
-    elsif params[:updated_at]
-      find_by(updated_at: params[:updated_at].to_datetime)
-    end
-  end
+  # def self.search(params)
+  #   if params[:name]
+  #     find_by(name: params[:name])
+  #   elsif params[:id]
+  #     find_by(id: params[:id])
+  #   elsif params[:created_at]
+  #     find_by(created_at: params[:created_at])
+  #   elsif params[:updated_at]
+  #     find_by(updated_at: params[:updated_at].to_datetime)
+  #   end
+  # end
 
   def self.most_revenue(quantity = 5)
     select("merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue")
