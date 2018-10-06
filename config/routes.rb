c8ac70b7fc4a71a3208079460c136fe67465eaa2
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-06 15:33:29 -0600

    Test & pass tests for invoice item item endpoint; closes #22

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
 
       namespace :invoice_items do
         get '/:id/invoice', to: 'invoice#show'
+        get '/:id/item', to: 'item#show'
       end
 
       resources :merchants, only: [:index, :show]
