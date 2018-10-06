68db5b2c5129fc996eef2361311e5f4e083bbcbb
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-06 15:32:20 -0600

    Align test with objective

--- spec/requests/api/v1/invoices/items_request_spec.rb
+++ spec/requests/api/v1/invoices/items_request_spec.rb
@@ -2,19 +2,21 @@ require 'rails_helper'
 
 describe 'Invoice Items API' do
   it 'sends a list of items for an invoice' do
-    id = create(:invoice).id
-    item_1, item_2, item_3, item_4 = create_list(:invoice_item, 4, invoice_id: id)
+    create(:item) # unrelated item
+    merchant_id = create(:merchant).id
+    items = create_list(:item, 4, merchant_id: merchant_id)
+    invoice_id = create(:invoice, merchant_id: merchant_id).id
 
-    get "/api/v1/invoices/#{id}/items"
+    items.map do |item|
+      create(:invoice_item, item_id: item.id, invoice_id: invoice_id)
+    end
+
+    get "/api/v1/invoices/#{invoice_id}/items"
 
     items = JSON.parse(response.body)
 
     expect(response).to be_successful
     expect(items.class).to eq(Array)
     expect(items.count).to eq(4)
-    expect(items[0]["id"]).to eq(item_1.id)
-    expect(items[1]["id"]).to eq(item_2.id)
-    expect(items[2]["id"]).to eq(item_3.id)
-    expect(items[3]["id"]).to eq(item_4.id)
   end
 end
