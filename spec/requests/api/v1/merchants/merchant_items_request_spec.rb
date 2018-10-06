63ac953446f5871a6d65b756a0bca0c810ac90ec
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-05 22:35:39 -0600

    Add expectations to test for retrieving a merchants items

--- spec/requests/api/v1/merchants/merchant_items_request_spec.rb
+++ spec/requests/api/v1/merchants/merchant_items_request_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-describe 'MerchantItems API' do
+describe 'Merchant Items API' do
   it 'sends a list of merchant items' do
     merchant = create(:merchant)
-    items = create_list(:item, 3, merchant_id: merchant.id, created_at: Time.now, updated_at: Time.now)
+    item_1, item_2, item_3 = create_list(:item, 3, merchant_id: merchant.id, created_at: Time.now, updated_at: Time.now)
 
     get "/api/v1/merchants/#{merchant.id}/items"
 
@@ -12,5 +12,8 @@ describe 'MerchantItems API' do
     expect(response).to be_successful
     expect(items.class).to eq(Array)
     expect(items.count).to eq(3)
+    expect(items[0]["id"]).to eq(item_1.id)
+    expect(items[1]["id"]).to eq(item_2.id)
+    expect(items[2]["id"]).to eq(item_3.id)
   end
 end
