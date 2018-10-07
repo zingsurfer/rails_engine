813aecf7673c6399ba3c2f34c9bba79484a902c3
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-07 10:05:07 -0600

    Add tests for find all by name & find all by id for merchants & make tests pass

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
 
       namespace :merchants do
         get '/find', to: 'search#show'
+        get '/find_all', to: 'search#index'
         get '/:id/items', to: 'items#index'
         get '/:id/invoices', to: 'invoices#index'
         get '/most_revenue', to: 'most_revenue#index'
