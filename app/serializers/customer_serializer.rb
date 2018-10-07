d4480bc8a6c49e8ee7136c01dac5874e143db539
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-06 23:58:35 -0600

    Align attributes vertically

--- app/serializers/customer_serializer.rb
+++ app/serializers/customer_serializer.rb
@@ -1,3 +1,5 @@
 class CustomerSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name
+  attributes :id,
+             :first_name,
+             :last_name
 end
--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,3 +1,4 @@
 class MerchantSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id,
+             :name
 end
