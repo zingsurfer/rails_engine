7a64dbf3beafc7ed839e5fde12f7ab0c13400c84
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-10-06 22:35:24 -0600

    Add expectations

--- spec/requests/api/v1/invoices/items_request_spec.rb
+++ spec/requests/api/v1/invoices/items_request_spec.rb
@@ -4,10 +4,10 @@ describe 'Invoice Items API' do
   it 'sends a list of items for an invoice' do
     create(:item) # unrelated item
     merchant_id = create(:merchant).id
-    items = create_list(:item, 4, merchant_id: merchant_id)
+    item_1, item_2, item_3 = create_list(:item, 3, merchant_id: merchant_id)
     invoice_id = create(:invoice, merchant_id: merchant_id).id
 
-    items.map do |item|
+    [item_1, item_2, item_3].map do |item|
       create(:invoice_item, item_id: item.id, invoice_id: invoice_id)
     end
 
@@ -17,6 +17,9 @@ describe 'Invoice Items API' do
 
     expect(response).to be_successful
     expect(items.class).to eq(Array)
-    expect(items.count).to eq(4)
+    expect(items.count).to eq(3)
+    expect(items[0]["id"]).to eq(item_1.id)
+    expect(items[1]["id"]).to eq(item_2.id)
+    expect(items[2]["id"]).to eq(item_3.id)
   end
 end
